3a31a5f75bad003a03d80d9a1f361d2bdf3f0203
bozangnj <67308237+bozangnj@users.noreply.github.com>
2020-09-18 14:42:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -21,9 +21,7 @@ async function doIt() {
 
   const files = await octokit.pulls.listFiles({owner, repo, pull_number});
   
-  for(file in files.data) {
-    console.log('${file.filename}');
-  }
+  files.data.forEach(i => console.log(`${i}`); );
   
   
   const matched = files.data
