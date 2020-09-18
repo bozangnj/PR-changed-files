ad2c233cbeaa665f9b62135c3ffd4136bcb1da03
bozangnj <67308237+bozangnj@users.noreply.github.com>
2020-09-18 13:54:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,11 @@ async function doIt() {
 
   const files = await octokit.pulls.listFiles({owner, repo, pull_number});
   
+  for(file in files.data) {
+    console.log(file.filename);
+}
+  
+  
   const matched = files.data
     .map(_ => _.filename)
     .some(filename =>
